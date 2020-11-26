40ad7ea46f66a814033112becc4b7cf7e2f17522
Armando-Castillo <armandocreyes02@gmail.com>
2020-11-25 21:21:24 -0600

    ultimo https

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ var firebaseConfig = {
     storageBucket: "portafolio-88ce3.appspot.com",
     messagingSenderId: "1047604769765",
     appId: "1:1047604769765:web:ee8195413eeb0e79c3350c"
-}
+};
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-const form = document.getElementById('formulario')
+const form = document.getElementById('formulario');
 
 const firestore = firebase.firestore();
 
-const docRef = firestore.collection("colaboraciones")
+const docRef = firestore.collection("colaboraciones"); 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
     var nombreVar = document.getElementById('nombre').value;
@@ -30,4 +30,4 @@ form.addEventListener('submit', (e)=> {
     })
     .catch((e)=>{console.log(e)})
     console.log(nombre, telefono, mensaje)
-})
+});
